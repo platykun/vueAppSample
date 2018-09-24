1d498351239ca1749d41f7e615c71dcdcc12cf04
platykun <nagonago561@yahoo.co.jp>
2018-09-24 10:50:31 +0900

    URLの指定方法見直し

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,5 +1,5 @@
 'use strict';
 module.exports = {
   NODE_ENV: '"production"',
-  API_URL: '"https://board-management-api.herokuapp.com/"',
+  API_URL: '"https://board-management-api.herokuapp.com"',
 };
